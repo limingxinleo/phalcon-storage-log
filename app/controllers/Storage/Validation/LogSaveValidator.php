74a64dbd7805c9e59bd1e436226365d218b5b12e
李铭昕 <715557344@qq.com>
2017-09-27 17:18:44 +0800

    * 修改Validator

--- app/controllers/Storage/Validation/LogSaveValidator.php
+++ app/controllers/Storage/Validation/LogSaveValidator.php
@@ -10,15 +10,30 @@ namespace App\Controllers\Storage\Validation;
 
 use App\Core\Validation\Validator;
 use Phalcon\Validation\Validator\PresenceOf;
+use Phalcon\Validation\Validator\Date;
 
 class LogSaveValidator extends Validator
 {
     public function initialize()
     {
         $this->add(
-            'module',
+            [
+                'module',
+                'data',
+                'time',
+            ],
             new PresenceOf([
-                'message' => 'The name is required',
+                'message' => 'The :field is required',
+            ])
+        );
+
+        $this->add(
+            [
+                'time',
+            ],
+            new Date([
+                "format" => "Y-m-d H:i:s",
+                "message" => "The :field is invalid",
             ])
         );
     }
